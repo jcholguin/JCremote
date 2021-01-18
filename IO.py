da128236c13866b13725770ab1db5dd4e781c194
JUAN CARLOS HOLGUIN SOTOMAYOR <JUAN.CARLOS.HOLGUIN.SOTOMAYOR1@ibm.com>
2021-01-18 16:19:04 -0600

    Adding IO file

---
+++ IO.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 17 22:21:49 2021
+
+@author: juancarlosholguinsotomayor
+"""
+
+from io import open
+fichero2 = open('fichero.txt', 'w')
+fichero = open('fichero.txt', 'r')
+
+text = "Como estas"
+fichero2.write(text)
+"""
+texto = fichero.read()
+print("Read: ", texto)
+"""
+fichero.seek(0)
+texto2 = fichero.readline()
+print("Readline: ", texto2)
+"""
+texto3 = fichero.readlines()
+print("Readlines: ", texto3)
+"""
+fichero2.close()
+fichero.close()
+
+
+
